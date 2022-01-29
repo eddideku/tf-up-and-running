9375f71e6f9358f8be11a6c11fcc71c0b771bc9d
Eddi Hughes <eddi.hughes@exprealty.net>
2022-01-29 13:42:01 -0800

    gitignore: ignore lock files; intro: fix typo on vpc variable

--- introduction-to-tf/single-web-server/main.tf
+++ introduction-to-tf/single-web-server/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws.security_group.instance.id}"]
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
   #!/bin/bash
