24c96ecc68c6a4d913f03df2fb2858380ed2770f
Eddi Hughes <eddi.hughes@exprealty.net>
2022-01-29 13:28:54 -0800

    intro: fix config-web-server tf 1.1.3 syntax errors, providers.tf personal aws profile/creds, added tfenv .terraform-version support

--- introduction-to-tf/config-web-server/main.tf
+++ introduction-to-tf/config-web-server/main.tf
@@ -1,30 +1,25 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "example" {
     ami = "ami-40d28157"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws.security_group.instance.id}"]
-    
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
     user_data = <<-EOF
     #!/bin/bash
     echo "Hello, Universe!" > index.html
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
-    
+
     tags = {
         Name = "terraform-web-server"
     }
 }
 
-
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
-    
+
     ingress {
-        from_port = ${var.server_port}
-        to_port = ${var.server_port}
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
